fcd0ea6b030e188cecf8985f9ed5cb16b28addd0
Dmitriy Kalinin <cppforlife@gmail.com>
2019-05-13 14:17:59 -0700

    add list command to list images

---
+++ pkg/kbld/cmd/list.go
@@ -0,0 +1,93 @@
+package cmd
+
+import (
+	"github.com/cppforlife/go-cli-ui/ui"
+	uitable "github.com/cppforlife/go-cli-ui/ui/table"
+	cmdcore "github.com/k14s/kbld/pkg/kbld/cmd/core"
+	ctlres "github.com/k14s/kbld/pkg/kbld/resources"
+	"github.com/spf13/cobra"
+)
+
+type ListOptions struct {
+	ui          ui.UI
+	depsFactory cmdcore.DepsFactory
+
+	FileFlags     FileFlags
+	RegistryFlags RegistryFlags
+}
+
+func NewListOptions(ui ui.UI, depsFactory cmdcore.DepsFactory) *ListOptions {
+	return &ListOptions{ui: ui, depsFactory: depsFactory}
+}
+
+func NewListCmd(o *ListOptions, flagsFactory cmdcore.FlagsFactory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "list",
+		Aliases: []string{"ls"},
+		Short:   "List images",
+		RunE:    func(_ *cobra.Command, _ []string) error { return o.Run() },
+	}
+	o.FileFlags.Set(cmd)
+	o.RegistryFlags.Set(cmd)
+	return cmd
+}
+
+func (o *ListOptions) Run() error {
+	rs, _, err := o.FileFlags.ResourcesAndConfig()
+	if err != nil {
+		return err
+	}
+
+	foundImages, err := o.findImages(rs)
+	if err != nil {
+		return err
+	}
+
+	table := uitable.Table{
+		Title:   "Images",
+		Content: "images",
+
+		Header: []uitable.Header{
+			uitable.NewHeader("Image"),
+			uitable.NewHeader("Resource"),
+		},
+
+		SortBy: []uitable.ColumnSort{
+			{Column: 0, Asc: true},
+			{Column: 1, Asc: true},
+		},
+
+		FillFirstColumn: false,
+	}
+
+	for _, imgSrc := range foundImages {
+		table.Rows = append(table.Rows, []uitable.Value{
+			uitable.NewValueString(imgSrc.Image),
+			uitable.NewValueString(imgSrc.Resource.Description()),
+		})
+	}
+
+	o.ui.PrintTable(table)
+
+	return nil
+}
+
+type foundSource struct {
+	Image    string
+	Resource ctlres.Resource
+}
+
+func (o *ListOptions) findImages(rs []ctlres.Resource) ([]foundSource, error) {
+	foundImages := []foundSource{}
+
+	for _, res := range rs {
+		visitValues(res.DeepCopyRaw(), imageKey, func(val interface{}) (interface{}, bool) {
+			if img, ok := val.(string); ok {
+				foundImages = append(foundImages, foundSource{Image: img, Resource: res})
+			}
+			return nil, false
+		})
+	}
+
+	return foundImages, nil
+}
