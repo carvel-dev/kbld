91a5653015367ce23f5bcc1089ed1af9bfba8c0c
Dmitriy Kalinin <cppforlife@gmail.com>
2020-03-06 14:33:14 -0500

    resolve image references twice to catch any registries that return "random" digests

--- pkg/kbld/image/resolved.go
+++ pkg/kbld/image/resolved.go
@@ -1,6 +1,8 @@
 package image
 
 import (
+	"fmt"
+
 	regname "github.com/google/go-containerregistry/pkg/name"
 )
 
@@ -31,6 +33,18 @@ func (i ResolvedImage) URL() (string, []ImageMeta, error) {
 	imgDescriptor, err := i.registry.Generic(tag)
 	if err != nil {
 		return "", nil, err
+	}
+
+	// Resolve image second time because some older registry can
+	// return "random" digests that change for every request.
+	// See https://github.com/k14s/kbld/issues/21 for details.
+	imgDescriptor2, err := i.registry.Generic(tag)
+	if err != nil {
+		return "", nil, err
+	}
+
+	if imgDescriptor.Digest.String() != imgDescriptor2.Digest.String() {
+		return "", nil, fmt.Errorf("Expected digest resolution to be consistent over two separate requests")
 	}
 
 	url, metas, err := NewDigestedImageFromParts(tag.Repository.String(), imgDescriptor.Digest.String()).URL()
