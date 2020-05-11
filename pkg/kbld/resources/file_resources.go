8655a90ac96f5b638de306912dff69f1594af43b
Dmitriy Kalinin <cppforlife@gmail.com>
2020-05-11 19:41:13 -0400

    improve yaml unmarshaling error message

--- pkg/kbld/resources/file_resources.go
+++ pkg/kbld/resources/file_resources.go
@@ -69,15 +69,15 @@ func (r FileResource) Description() string { return r.fileSrc.Description() }
 func (r FileResource) Resources() ([]Resource, error) {
 	docs, err := NewYAMLFile(r.fileSrc).Docs()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Parsing file '%s': %s", r.Description(), err)
 	}
 
 	var resources []Resource
 
-	for _, doc := range docs {
+	for i, doc := range docs {
 		rs, err := NewResourcesFromBytes(doc)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("Parsing file '%s' doc %d: %s", r.Description(), i+1, err)
 		}
 
 		resources = append(resources, rs...)
