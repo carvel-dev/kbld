0c7d6cf31e1da4dfdf03c4b10f5d36a32a83c38f
starptech <deusdustin@gmail.com>
2020-11-14 22:54:58 +0100

    better approach to maintain developer tools

--- hack/lint.sh
+++ hack/lint.sh
@@ -2,6 +2,5 @@
 
 set -e -x -u
 
-GO111MODULE=off go get -u github.com/kisielk/errcheck
 # add -verbose to see the resulting list of added excludes.
-${GOPATH:-$(go env GOPATH)}/bin/errcheck -exclude "$PWD/hack/errcheck_excludes.txt" ./pkg/kbld/...
+go run github.com/kisielk/errcheck -exclude "$PWD/hack/errcheck_excludes.txt" ./pkg/kbld/...
---
+++ tools/tools.go
@@ -0,0 +1,12 @@
+// +build tools
+
+// Go modules support developer tools (commands) as dependencies.
+// For example, your project might require a tool to help with code generation, or to lint/vet your code for correctness.
+// Adding developer tool dependencies ensures that all developers use the same version of each tool.
+
+package tools
+
+import (
+	// exists solely for its side effects, in this case the declaration of the dependency.
+	_ "github.com/kisielk/errcheck"
+)
