505daf6e400cfd21f9a59ed586076d1f4ae6eb6d
starptech <deusdustin@gmail.com>
2020-11-15 13:01:59 +0100

    add comment about flag

--- tools/tools.go
+++ tools/tools.go
@@ -1,5 +1,7 @@
 // +build tools
 
+// the build flag is needed to avoid side-effects of init() functions in tool dependencies.
+
 // Go modules support developer tools (commands) as dependencies.
 // For example, your project might require a tool to help with code generation, or to lint/vet your code for correctness.
 // Adding developer tool dependencies ensures that all developers use the same version of each tool.
