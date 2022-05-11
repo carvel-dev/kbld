7233373726161882e8762ad654218cae27ac2511
Dmitriy Kalinin <dkalinin@vmware.com>
2022-05-11 11:08:44 -0400

    switch ci to linux and configure registry tls

--- hack/test-all-minikube-local-registry.sh
+++ hack/test-all-minikube-local-registry.sh
@@ -7,6 +7,30 @@ export KBLD_E2E_DOCKERHUB_HOSTNAME=${KBLD_E2E_DOCKERHUB_HOSTNAME:-`minikube ip`}
 # uncomment to disable stress tests
 # export KBLD_E2E_SKIP_STRESS_TESTS=true
 
-kapp deploy -a reg -f test/e2e/assets/minikube-local-registry.yml -y
+# Install secretgen to generate registry certs
+kapp deploy -a sg -f https://github.com/vmware-tanzu/carvel-secretgen-controller/releases/download/v0.8.0/release.yml -y
+# Install local docker2 registry
+kapp deploy -a reg -f <(ytt -f test/e2e/assets/minikube-local-registry.yml -v registry_alt_name=$(minikube ip)) -y
+
+if [ ! -z "$GITHUB_RUN_ID" ]; then
+  # Install registry ca cert on the host machine
+  kubectl get secret registry-ca-cert -ojsonpath='{.data.crt\.pem}' | base64 --decode > registry-ca-cert.crt
+  sudo cp registry-ca-cert.crt /usr/local/share/ca-certificates/
+  sudo update-ca-certificates
+
+  # Docker needs its own CA cert configuration
+  sudo mkdir -p /etc/docker/certs.d/${KBLD_E2E_DOCKERHUB_HOSTNAME}
+  sudo cp registry-ca-cert.crt /etc/docker/certs.d/${KBLD_E2E_DOCKERHUB_HOSTNAME}/ca.crt
+
+  # Buildkit needs to talk to above registry however
+  # it does not seem to properly auto-copy CA certificates
+  # so disable certificate verification
+  cat <<EOF >buildkitd.toml
+[registry."${KBLD_E2E_DOCKERHUB_HOSTNAME}"]
+  insecure = true
+EOF
+  # Need to bootstrap to avoid race conditions to boot
+  docker buildx create minikube --use --driver=kubernetes --bootstrap --config buildkitd.toml
+fi
 
 ./hack/test-all.sh $@
