bbbfc0126d59bf9f980504023f538fffcb3c9a51
Dmitriy Kalinin <dkalinin@vmware.com>
2022-06-06 12:45:20 -0400

    run without sudo in the container

--- hack/test-all-minikube-local-registry.sh
+++ hack/test-all-minikube-local-registry.sh
@@ -14,12 +14,12 @@ kapp deploy -a reg -f <(ytt -f test/e2e/assets/minikube-local-registry.yml -v re
 
 # Install registry ca cert on the host machine
 kubectl get secret registry-ca-cert -ojsonpath='{.data.crt\.pem}' | base64 --decode > registry-ca-cert.crt
-sudo cp registry-ca-cert.crt /usr/local/share/ca-certificates/
-sudo update-ca-certificates
+cp registry-ca-cert.crt /usr/local/share/ca-certificates/
+update-ca-certificates
 
 # Docker needs its own CA cert configuration
-sudo mkdir -p /etc/docker/certs.d/${KBLD_E2E_DOCKERHUB_HOSTNAME}
-sudo cp registry-ca-cert.crt /etc/docker/certs.d/${KBLD_E2E_DOCKERHUB_HOSTNAME}/ca.crt
+mkdir -p /etc/docker/certs.d/${KBLD_E2E_DOCKERHUB_HOSTNAME}
+cp registry-ca-cert.crt /etc/docker/certs.d/${KBLD_E2E_DOCKERHUB_HOSTNAME}/ca.crt
 
 # Buildkit needs to talk to above registry however
 # it does not seem to properly auto-copy CA certificates
