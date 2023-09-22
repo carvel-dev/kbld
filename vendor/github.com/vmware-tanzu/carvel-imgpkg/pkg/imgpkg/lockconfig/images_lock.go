09b805527d91d4547038d32369cd5d3f4fa80083
sethiyash <yashsethiya97@gmail.com>
2023-09-22 11:41:35 +0530

    Bumping imgpkg to v0.38.0 and vendir to v0.35.0
    Signed-off-by: sethiyash <yashsethiya97@gmail.com>

--- vendor/github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/lockconfig/images_lock.go
+++ vendor/github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/lockconfig/images_lock.go
@@ -5,7 +5,7 @@ package lockconfig
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 
 	regname "github.com/google/go-containerregistry/pkg/name"
 	"sigs.k8s.io/yaml"
@@ -37,7 +37,7 @@ func NewEmptyImagesLock() ImagesLock {
 }
 
 func NewImagesLockFromPath(path string) (ImagesLock, error) {
-	bs, err := ioutil.ReadFile(path)
+	bs, err := os.ReadFile(path)
 	if err != nil {
 		return ImagesLock{}, fmt.Errorf("Reading path %s: %s", path, err)
 	}
@@ -125,7 +125,7 @@ func (i ImagesLock) WriteToPath(path string) error {
 		return err
 	}
 
-	err = ioutil.WriteFile(path, bs, 0600)
+	err = os.WriteFile(path, bs, 0600)
 	if err != nil {
 		return fmt.Errorf("Writing images config: %s", err)
 	}
