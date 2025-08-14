357683d52fd07e6de620ec8a0d2e61262cd7e08a
Vishrant Gupta <gvishrant@vmware.com>
2025-08-13 21:44:32 -0500

    #527 - Adding structure to support maven jib build
    Signed-off-by: Qi Hu <qi.hu@broadcom.com>

--- pkg/kbld/builder/maven/jib.go
+++ pkg/kbld/builder/maven/jib.go
@@ -1,10 +1,19 @@
 package maven
 
 import (
+	"bytes"
 	ctlbdk "carvel.dev/kbld/pkg/kbld/builder/docker"
 	"carvel.dev/kbld/pkg/kbld/config"
 	ctllog "carvel.dev/kbld/pkg/kbld/logger"
+	"fmt"
+	"io"
+	"os/exec"
+	"path/filepath"
+	"regexp"
 )
+
+var defaultImageTag = "latest"
+var ImageID = regexp.MustCompile("(sha256:)([0-9a-z]+)")
 
 type Jib struct {
 	docker ctlbdk.Docker
@@ -17,7 +26,54 @@ func NewMavenJib(docker ctlbdk.Docker, logger ctllog.Logger) Jib {
 
 func (b *Jib) Run(image, directory string, opts config.SourceJibRunOpts) (ctlbdk.TmpRef, error) {
 
-	// TODO add logic to run maven jib build command
+	prefixedLogger := b.logger.NewPrefixedWriter(image + " | ")
 
-	return ctlbdk.TmpRef{}, nil
+	prefixedLogger.Write([]byte(fmt.Sprintf("starting build (using bazel): %s\n", directory)))
+	defer prefixedLogger.Write([]byte("finished build (using bazel)\n"))
+
+	tag := opts.Tag
+	if tag == nil {
+		tag = &defaultImageTag
+	}
+	targetImage := fmt.Sprintf("%s:%s", image, *tag)
+
+	var stdoutBuf, stderrBuf bytes.Buffer
+
+	if opts.Target == nil {
+		return ctlbdk.TmpRef{}, fmt.Errorf("Expected target to be specified, but was not")
+	}
+
+	// Base arguments for the Maven Jib command.
+	cmdArgs := []string{
+		"compile",
+		"jib:dockerBuild",
+		"-Dimage=" + targetImage,
+	}
+
+	if opts.RawOptions != nil {
+		cmdArgs = append(cmdArgs, *opts.RawOptions...)
+	}
+
+	cmd := exec.Command("mvn", cmdArgs...)
+
+	cmd.Dir = filepath.Join(directory, *opts.Target)
+	cmd.Stdout = io.MultiWriter(&stdoutBuf, prefixedLogger)
+	cmd.Stderr = io.MultiWriter(&stderrBuf, prefixedLogger)
+
+	prefixedLogger.Write([]byte(fmt.Sprintf("running command: %s\n", cmd)))
+
+	if err := cmd.Run(); err != nil {
+		prefixedLogger.Write([]byte(fmt.Sprintf("error: %s\n", err)))
+		return ctlbdk.TmpRef{}, err
+	}
+
+	inspectData, err := b.docker.Inspect(targetImage)
+	if err != nil {
+		prefixedLogger.Write([]byte(fmt.Sprintf("inspect error: %s\n", err)))
+		return ctlbdk.TmpRef{}, err
+	}
+
+	prefixedLogger.Write([]byte(fmt.Sprintf("digest: %s, id: %s\n", inspectData.RepoDigests, inspectData.ID)))
+
+	return b.docker.RetagStable(ctlbdk.NewTmpRef(inspectData.ID), image, inspectData.ID, prefixedLogger)
 }
